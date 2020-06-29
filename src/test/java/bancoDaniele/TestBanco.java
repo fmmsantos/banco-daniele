5a23157a66699ba2527260c12b2b108b0e10c3e9
fmmsantos <45079131+fmmsantos@users.noreply.github.com>
2020-06-29 10:47:47 -0300

    teste funcionando

--- src/test/java/bancoDaniele/TestBanco.java
+++ src/test/java/bancoDaniele/TestBanco.java
@@ -13,29 +13,33 @@ import org.junit.Test;
 public class TestBanco {
 	
 	@Test
-	public void resultadoBanco() {
-		TituloDTO banco = new TituloDTO("1233", LocalDate.of(2019, 4, 10), new BigDecimal("700"), StatusTitulo.ATRASO);
-		TituloDTO banco1 = new TituloDTO("1234", LocalDate.of(2019, 5, 15), new BigDecimal("800"), StatusTitulo.PAGO);
-		TituloDTO banco2 = new TituloDTO("1235", LocalDate.of(2019, 6, 20), new BigDecimal("600"), StatusTitulo.PAGO);
-
-		TituloDTO serasa = new TituloDTO("1233", LocalDate.of(2019, 4, 10), new BigDecimal("700"), StatusTitulo.ATRASO);
-		TituloDTO serasa1 = new TituloDTO("1234", LocalDate.of(2019, 5, 15), new BigDecimal("800"), StatusTitulo.PAGO);
-		TituloDTO serasa2 = new TituloDTO("1235", LocalDate.of(2019, 6, 20), new BigDecimal("600"),StatusTitulo.ATRASO);
-
-		List<TituloDTO> serasas = new ArrayList<>();
-		Arrays.asList(serasa, serasa1, serasa2);
-
-		List<TituloDTO> bancos = new ArrayList<>();
-		Arrays.asList(banco, banco1, banco2);
+	public void TestNovo() {
+		List<TituloDTO> titulosBanco = Arrays.asList(
+		 new TituloDTO("234", LocalDate.of(2019, 4, 10), new BigDecimal("700"), StatusTitulo.ATRASO),
+		 new TituloDTO("245", LocalDate.of(2019, 5, 15), new BigDecimal("825"), StatusTitulo.PAGO),
+		 new TituloDTO("256", LocalDate.of(2019, 6, 20), new BigDecimal("380"), StatusTitulo.PAGO),
+		 new TituloDTO("257", LocalDate.of(2019, 6, 20), new BigDecimal("250"), StatusTitulo.PAGO)
+);
+		List<TituloDTO> titulosSerasa = Arrays.asList(
+		 new TituloDTO("234", LocalDate.of(2019, 4, 10), new BigDecimal("700"), StatusTitulo.ATRASO),
+		 new TituloDTO("245", LocalDate.of(2019, 5, 15), new BigDecimal("825"), StatusTitulo.ATRASO),
+		 new TituloDTO("256", LocalDate.of(2019, 6, 20), new BigDecimal("380"),StatusTitulo.ATRASO));
 		
 		SerasaConciliacaoService service = new SerasaConciliacaoServiceImpl();
-		List<ConciliacaoDTO> output = service.conciliar(bancos, serasas);
+		List<ConciliacaoDTO> output = service.conciliar(titulosBanco, titulosSerasa);
 		
-		Assert.assertEquals("Deve retorna um titulo diferente da conciliacao",1,output.size());
-		Assert.assertEquals("O numero do documento � ","1235",output.get(2).getNrDocumento());
-		Assert.assertEquals("O valor do documento � ",new BigDecimal("600"),output.get(2).getValor());
-		Assert.assertEquals("O status do documento do banco ",StatusTitulo.PAGO,output.get(2).getStatusBanco());
-		Assert.assertEquals("O status do documento do serasa ",StatusTitulo.ATRASO,output.get(2).getStatusSerasa());
+		
+		ConciliacaoDTO conciliacao1 = output.get(0);
+		
+		Assert.assertEquals("O numero do documento é ", "245", conciliacao1.getNrDocumento());
+		Assert.assertEquals("O valor do documento é ", new BigDecimal("825"), conciliacao1.getValor());
+		Assert.assertEquals("O status do documento do banco ", StatusTitulo.PAGO, conciliacao1.getStatusBanco());
+		Assert.assertEquals("O status do documento do serasa ", StatusTitulo.ATRASO, conciliacao1.getStatusSerasa());
+		
+		ConciliacaoDTO conciliacao2 = output.get(1);
+		Assert.assertEquals("O numero do documento é ", "256", conciliacao2.getNrDocumento());
+		Assert.assertEquals("O valor do documento é ", new BigDecimal("380"), conciliacao2.getValor());
+		Assert.assertEquals("O status do documento do banco ", StatusTitulo.PAGO, conciliacao2.getStatusBanco());
+		Assert.assertEquals("O status do documento do serasa ", StatusTitulo.ATRASO, conciliacao2.getStatusSerasa());
 	}
-
 }
